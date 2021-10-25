14d030498a5f4fcc88f0c8461b25a88a453a4d0f
Matteo <m.isoldi.secondary@tutanota.com>
2021-10-25 21:52:04 +0200

    Done R2.

--- OOP_LAB_University/src/university/University.java
+++ OOP_LAB_University/src/university/University.java
@@ -8,56 +8,91 @@ package university;
  */
 public class University {
 
-	String universityName;
-	String rectorName;
-	String rectorSurname;
+	private final static int FIRST_ID_STUDENT = 10000;
+	private final static int MAX_STUDENTS = 1000;
+	private final static int MAX_COURSES = 50;
+	
+	private class Student{
+		String name;
+		String surname;
+		
+		public Student(String first, String last){
+			
+			this.name = first;
+			this.surname = last;
+			
+		}
+		
+		public String getName() {
+			
+			return this.name;
+		}
+		
+		public String getSurname() {
+					
+			return this.surname;
+		}
+	}
+	
+	private String universityName;
+	private String rectorName;
+	private String rectorSurname;
+	private int enrolledStudents;
+	private Student[] students;	
 	
 	//Constructor
 	public University(String name){
+		
 		this.universityName  = name;
 		this.rectorName = "";
 		this.rectorSurname = "";
+		this.enrolledStudents = 0;
+		this.students = new Student[MAX_STUDENTS];
+		
 	}
 	
 	// Getter for the name of the university
 	public String getName(){
+		
 		return this.universityName;
 	}
 	
 	// Defines the rector for the university
 	public void setRector(String first, String last){
+		
 		this.rectorName = first;
 		this.rectorSurname = last;
+		
 	}
 	
 	// Retrieves the rector of the university
 	public String getRector(){
+		
 		return (this.rectorName + " " + this.rectorSurname);
 	}
 	
-	/**
-	 * Enrol a student in the university
-	 * 
-	 * @param first first name of the student
-	 * @param last last name of the student
-	 * 
-	 * @return unique ID of the newly enrolled student
-	 */
+	// Enroll a student in the university
 	public int enroll(String first, String last){
-		//TODO: to be implemented
-		return -1;
+		
+		int newId;
+		Student newStudent;
+		
+		newId = this.enrolledStudents;	
+		newStudent = new Student(first, last);
+		this.students[newId] = newStudent;
+		this.enrolledStudents++;
+		
+		newId = newId + FIRST_ID_STUDENT;		
+		return newId;
 	}
 	
-	/**
-	 * Retrieves the information for a given student
-	 * 
-	 * @param id the ID of the student
-	 * 
-	 * @return information about the student
-	 */
+	// Retrieves the information for a given student
 	public String student(int id){
-		//TODO: to be implemented
-		return null;
+		
+		int recoverId;
+		
+		recoverId = id - FIRST_ID_STUDENT;
+		return (this.students[recoverId].getName() + " " + this.students[recoverId].getSurname());
 	}
 	
 	/**
